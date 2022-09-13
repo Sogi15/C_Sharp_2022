154089714700614cddd360abb89aedfb06b7591d
Sogi15 <90307491+Sogi15@users.noreply.github.com>
2022-09-13 11:51:30 +0200

    GitHubTeszt

---
+++ GithubTeszt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTeszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdm = new Random();
+            Console.WriteLine("40db véletlenszám!");
+            for (int i = 0; i <= 40; i++)
+            {
+                Console.Write($"{rdm.Next(-100,100)} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
