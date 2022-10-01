aef72f3a8f9dce3529de8d8068ac96910bd114c1
Attila Szunyogh <2021304@dszcberegszaszi.hu>
2022-10-01 08:01:54 +0200

    not works.

---
+++ Class_practice/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practice
+{
+    class Person
+    {
+        private string name, phoneNumber, emailAddress;
+
+        public Person(string name, string phonenNumber, string emailAddress)
+        {
+            this.name = name;
+            this.phoneNumber = phonenNumber;
+            this.emailAddress = emailAddress;
+        }
+        
+        public void data()
+        {
+                Console.WriteLine($"Név: {this.name}, Telefon: {this.phoneNumber}, E-mail: {this.emailAddress}");
+        }
+
+    }
+}
---
+++ Class_practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person teszt = new Person("Teszt Elek","06202460477","tesztelek@gmail.com");
+            teszt.data();
+            Console.ReadKey();
+        }
+    }
+}
